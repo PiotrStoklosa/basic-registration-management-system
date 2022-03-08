c7c35d5c2806c4441af3d52fc942ee486c65cc1b
PiotrStoklosa <piotrjstoklosa@gmail.com>
2022-03-08 21:28:38 +0100

    Initial commit

---
+++ src/test/java/com/fingo/registration/RegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fingo.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
